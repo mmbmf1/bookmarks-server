cec52d3da1c5e0200785aa1ea28fcb039e9ab952
Michael <mmbmf.mm@gmail.com>
2019-12-09 08:24:35 -0600

    initial bookmarks route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const NODE_ENV = require("./config");
+const bookmarkRouter = require('../bookmarks/bookmarks-router');
 
 const app = express();
 
@@ -15,9 +16,18 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if(!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
 });
+
+app.use(bookmarkRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
