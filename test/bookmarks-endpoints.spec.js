0f4a428eb0443370f068d182ff4a7751e956c2cd
Michael <mmbmf.mm@gmail.com>
2020-01-13 17:58:05 -0600

    changes for deploy

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -8,7 +8,7 @@ describe("Bookmarks Endpoints", () => {
   before("make knex instance", () => {
     db = knex({
       client: "pg",
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set("db", db);
   });
